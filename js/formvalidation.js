51b6da9610f2a88b46ff2c9e63ee90d1ef3f091b
Orlando J Betancourth <obetancourthunicah@gmail.com>
2025-03-26 18:47:34 -0600

    feat: formularios mensaje de error

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -4,22 +4,54 @@ document.addEventListener('DOMContentLoaded', () => {
     const formSolicitud = document.getElementById("formSolicitud");
     const txtPrimerNombre = document.getElementById("txtPrimerNombre");
     const txtApellidoPaterno = document.getElementById("txtApellidoPaterno");
+    // const divPrimerNombre = document.getElementById("divPrimerNombre");
+    // const divApellidoPaterno = document.getElementById("divApellidoPaterno");
 
     function onSubmit_formSolicitud (e) {
         const errors = {};
         let hasErrors = false;
+
+        txtPrimerNombre.classList.remove('error');
+        txtApellidoPaterno.classList.remove('error');
+
         if (regexIsEmpty.test(txtPrimerNombre.value)){
             hasErrors = true;
-            errors["txtPrimerNombreError"] = "El nombre no puede ir vacío.";
+            errors["PrimerNombre"] = "El nombre no puede ir vacío.";
+        }
+
+        if (regexIsEmpty.test(txtApellidoPaterno.value)){
+            hasErrors = true;
+            errors["ApellidoPaterno"] = "El apellido no puede ir vacío.";
         }
 
         if (hasErrors) {
-            alert (
-                JSON.stringify(errors, null, 2)
-            );
+            const errorKeys = Object.keys(errors);
+            let focusAssigned = false;
+            for (errorKey of errorKeys) {
+                const errorDivHolder = document.getElementById(`div${errorKey}`);
+                if(errorDivHolder) {
+                    let errorDivMessage = document.getElementById(`${errorKey}Error`);
+                    if (errorDivMessage) {
+                        errorDivMessage.textContent = errors[errorKey];
+                    } else {
+                        errorDivMessage = document.createElement("DIV");
+                        errorDivMessage.textContent = errors[errorKey];
+                        errorDivMessage.classList.add('error');
+                        errorDivHolder.appendChild(errorDivMessage);
+                    }
+                    let inputObject = errorDivHolder.querySelector('input');
+                    inputObject.classList.add('error');
+                    if( !focusAssigned) {
+                        inputObject.focus();
+                    }
+                }
+            }
+            // alert (
+            //     JSON.stringify(errors, null, 2)
+            // );
+            e.preventDefault();
+            e.stopPropagation();
         }
-        e.preventDefault();
-        e.stopPropagation();
     }
 
 
